b3367391c265b884eea4f5dce76fd3f23ea57a6d
Vusal <iamvusalalakbarov@gmail.com>
2025-01-10 22:16:51 +0400

    implement medical market section with swiper functionality and styling

--- src/main.js
+++ src/main.js
@@ -1,1 +1,15 @@
+import Swiper from 'swiper';
+import { Navigation } from 'swiper/modules';
+import 'swiper/css';
 import './styles/main.scss';
+
+const medicalMarketSwiper = new Swiper('.medical-market-swiper', {
+  modules: [Navigation],
+  slidesPerView: 'auto',
+  spaceBetween: 20,
+  navigation: {
+    prevEl: '.medical-market-swiper-button.prev',
+    nextEl: '.medical-market-swiper-button.next',
+    disabledClass: 'disabled',
+  },
+});

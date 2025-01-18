cbfb6aa06fe8bac5135cd31cc2c999158a4ed8f6
Vusal <iamvusalalakbarov@gmail.com>
2025-01-18 14:47:04 +0400

    Remove Vercel configuration and add Vite configuration for routing and build setup

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        about: 'about.html',
+        catalog: 'catalog.html',
+        company: 'company.html',
+        news: 'news.html',
+        newsInner: 'news-inner.html',
+      },
+    },
+  },
+});
